adbe6bf71ba282e184bac713b71623d7341a4840
saisumughis <sai.sumughi@gmail.com>
2017-01-25 16:10:13 -0500

    components and services added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {TodoDataService} from './todo-data.service';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+newTodo: Todo = new Todo();
+
+ constructor(private TodoDataService: TodoDataService){
+
+ }
+
+ toggleTodoComplete(todo){
+   this.TodoDataService.toggleTodoComplete(todo);
+ }
+
+ addTodo(){
+   this.TodoDataService.addTodo(this.newTodo);
+   this.newTodo = new Todo();
+ }
+
+ removeTodo(todo){
+
+ }
+
+
 }
---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {Todo} from './todo';
+
+@Injectable()
+export class TodoDataService {
+
+ lastId: number = 0;
+ todos: Todo[] = [];
+  constructor() { }
+
+  addTodo(todo :Todo) :TodoDataService{
+    if(!todo.id){
+      todo.id = ++this.lastId;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  deleteTodoById(id:number) :TodoDataService{
+     this.todos = this.todos.filter(todo => todo.id !== id);
+     return this;
+  }
+
+  updateTodoById(id:number, values: Object = {}):Todo{
+    let todo = this.getTodoById(id);
+    if(!todo){
+      return null;
+    }
+    (<any>Object).assign(todo, values);
+    return todo;
+  }
+
+  getTodoById(id: number):Todo {
+    return this.todos.filter(todo => todo.id === id).pop();
+  }
+
+  getAllTodos() :Todo[]{
+    return this.todos;
+  }
+
+  toggleTodoComplete(todo :Todo){
+    let updatedTodo = this.updateTodoById(todo.id, {
+      complete: !todo.complete
+    });
+    return updatedTodo;
+  }
+
+}
